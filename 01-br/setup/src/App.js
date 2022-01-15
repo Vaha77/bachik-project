57bb6b6b461de131d7b1ac6e4e1639c587b6f131
vaha77 <abdulvohid996@gmail.com>
2022-01-16 02:03:33 +0900

    big

---
+++ 01-br/setup/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+  const [people, setPople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} bridays tody</h3>
+        <List people={people} />
+        <button onClick={() => setPople([])}>clear all</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
